2f4c27fdf8ff395acfb5ae846361da2d01f5243b
Steinberger <rsteinberger@tresys.com>
2020-12-16 12:55:20 -0500

    update

--- src/main/scala/com/demo/app/lib.scala
+++ src/main/scala/com/demo/app/lib.scala
@@ -1,8 +1,29 @@
 package com.demo.app
 
+import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
+import java.io.IOException
+
 class LibClass(files: Array[String]) {
     def libFunction() = {
         println("libFunction called")
+    }
+}
+
+class LibFileClass() {
+    def libFileAppendFunction(files: Array[File]) = {
+        println("libFileAppendFunction called")
+        println("files: " + files.size)
+        val sourceFile = files(0)
+		try { 
+		    // Open given file in append mode. 
+		    var out = new BufferedWriter(new FileWriter(sourceFile, true)); 
+		    out.write("TEST"); 
+		    out.close(); 
+		} catch {
+			case e: IOException => println("IOException occoured"); 
+		}
     }
 }
 
